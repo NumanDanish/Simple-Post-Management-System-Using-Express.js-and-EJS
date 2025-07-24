421785379925f03df44be07c1f9185a7d9d8ced5
Numan Danish <noman.danish7777@gmail.com>
2025-07-25 03:42:26 +0500

    Update Post added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-
 const path = require("path");  // for accessing other folders from indes.js
 
-app.use(express.urlencoded({extended: true}));  // for parsing of form data 
+const methodOverride = require('method-override')
 
+app.use(express.urlencoded({extended: true}));  // for parsing of form data 
+app.use(methodOverride('_method'))
 app.set("view engine","ejs");  // use EJS as template design 
 app.set("views",path.join(__dirname,"views"));  // build the path to view folder
 
@@ -63,4 +64,12 @@ app.get("/posts/:id/edit",(req,res)=>{
     let {id} = req.params;
     let post = posts.find((p)=> id === p.id);
     res.render("edit.ejs", {post});
-})
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> p.id === id );
+    post.content = newContent;
+    res.redirect("/posts");
+});
